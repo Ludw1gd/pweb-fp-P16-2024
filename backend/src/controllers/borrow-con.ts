387805fb11dbd78185fcbdf0e7607ea92241744f
rafhab <146955598+rafhab@users.noreply.github.com>
2024-12-27 23:03:15 +0700

    Update borrow-con.ts

--- backend/src/controllers/borrow-con.ts
+++ backend/src/controllers/borrow-con.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import { BorrowItem } from "../models/borrow-item";
 
-export const getBorrowItems = (req: Request, res: Response) => {
-  res.status(200).json(BorrowItem);
+export const getBorrowItems = async (req: Request, res: Response) => {
+  try {
+    const borrowItems = await BorrowItem.find(); 
+    res.status(200).json(borrowItems);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching borrowed items', error });
+  }
 };
