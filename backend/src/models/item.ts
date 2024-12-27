a14b3adb420d7f0659777e4b312ea58089606872
rafhab <146955598+rafhab@users.noreply.github.com>
2024-12-27 22:48:02 +0700

    Update item.ts

--- backend/src/models/item.ts
+++ backend/src/models/item.ts
@@ -5,13 +5,15 @@ interface IItem extends Document {
     amount: number;
     condition: string;
     created_at: Date;
+    updated_at: Date;
 }
 
 const ItemSchema: Schema = new Schema({
     name: { type: String, required: true },
     amount: { type: Number, required: true },
     condition: { type: String, required: true },
-    created_at: { type: Date, default: Date.now }
+    created_at: { type: Date, default: Date.now },
+    updated_at; { type: Date, default: Date.now},
 });
 
 export const Item = mongoose.model<IItem>('Item', ItemSchema);
